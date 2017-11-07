1af9cb430f23b3e805579a32c3cbc28d559b4e30
lixinze <l7072184920@gmail.com>
2017-11-07 00:07:57 -0600

    added comment

--- app/src/main/java/com/example/lixinze/csc250_hw11_template/MainActivity.java
+++ app/src/main/java/com/example/lixinze/csc250_hw11_template/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //xinze li
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
